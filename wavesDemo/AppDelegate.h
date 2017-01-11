a27cb7ab508fe09326fe561e65ea6e7c4ce92fce
董知樾 <dongzhiyue@dongzhiyuedeMacBook-Pro.local>
2017-01-11 16:15:34 +0800

    Initial Commit

---
+++ wavesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wavesDemo
+//
+//  Created by 董知樾 on 2017/1/11.
+//  Copyright © 2017年 董知樾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wavesDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wavesDemo
+//
+//  Created by 董知樾 on 2017/1/11.
+//  Copyright © 2017年 董知樾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
