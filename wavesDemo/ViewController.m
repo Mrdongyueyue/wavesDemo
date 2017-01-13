50f39c0bbe1d702303a8153b5d778f4599b661ff
董知樾 <dongzhiyue@dongzhiyuedeMacBook-Pro.local>
2017-01-13 20:19:29 +0800

    渐变色

--- wavesDemo/ViewController.m
+++ wavesDemo/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2017年 董知樾. All rights reserved.
 //
 
+#define color(r,g,b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -20,6 +22,10 @@
 
 @property (strong, nonatomic) UIBezierPath *path2;
 
+@property (strong, nonatomic) CAGradientLayer *gradientLayer;
+
+@property (strong, nonatomic) CAGradientLayer *gradientLayer2;
+
 @end
 
 @implementation ViewController
@@ -29,21 +35,15 @@
     
     _shapeLayer = [CAShapeLayer layer];
     _shapeLayer.frame = CGRectMake(0, 100, 375, 150);
-    [self.view.layer addSublayer:_shapeLayer];
     
     _shapeLayer2 = [CAShapeLayer layer];
     _shapeLayer2.frame = CGRectMake(0, 100, 375, 150);
-    [self.view.layer addSublayer:_shapeLayer2];
-    
-//    _shapeLayer.strokeColor = [UIColor orangeColor].CGColor;
-//    _shapeLayer2.strokeColor = [UIColor cyanColor].CGColor;
-    
-    _shapeLayer.fillColor = [[UIColor orangeColor] colorWithAlphaComponent:0.3].CGColor;
-    _shapeLayer2.fillColor = [[UIColor cyanColor] colorWithAlphaComponent:0.3].CGColor;
     
     _displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(drawPath)];
     [_displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
     
+    [self addGradient1];
+    [self addGradient2];
 }
 
 - (void)drawPath {
@@ -83,7 +83,102 @@
     if (i > M_PI * 2) {
         i = 0;//防止i越界
     }
+    _gradientLayer.mask = _shapeLayer;
+    _gradientLayer2.mask = _shapeLayer2;
 }
+
+- (void)addGradient1 {
+    //139 70 83
+    //174 252 217
+    _gradientLayer = [CAGradientLayer layer];
+    NSInteger count = 20;
+    NSMutableArray *colors = [NSMutableArray array];
+    NSMutableArray *locations = [NSMutableArray array];
+    for (NSInteger i = 0; i < count ; i ++) {
+        UIColor *color = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:0.3];
+        [colors addObject:(__bridge id)color.CGColor];
+        [locations addObject:@(i /(CGFloat)count)];
+    }
+    //颜色数组
+    _gradientLayer.colors = colors;
+    //可以不设置
+    _gradientLayer.locations = locations;
+    //startPoint endPoint 确定条纹方向 不设置 默认水平默认值[.5,0]和[.5,1]
+    _gradientLayer.startPoint = CGPointMake(0, 0);
+    _gradientLayer.endPoint = CGPointMake(1, 1);
+    
+    _gradientLayer.type = kCAGradientLayerAxial;
+    
+    _gradientLayer.frame = CGRectMake(0, 0, 375, 150);
+    
+    [self.view.layer addSublayer:_gradientLayer];
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"colors"];
+    
+    animation.duration = 2;
+    
+    animation.repeatCount = MAXFLOAT;
+    
+    NSMutableArray *toValue = [NSMutableArray array];
+    
+    for (NSInteger i = 0; i < count ; i ++) {
+        
+        UIColor *color = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:0.3];
+        
+        [toValue addObject:(__bridge id)color.CGColor];
+        
+    }
+    animation.autoreverses = YES;
+    animation.toValue = toValue;
+    [_gradientLayer addAnimation:animation forKey:@"gradientLayer"];
+    
+}
+- (void)addGradient2 {
+    
+    _gradientLayer2 = [CAGradientLayer layer];
+    NSInteger count = 20;
+    NSMutableArray *colors = [NSMutableArray array];
+    NSMutableArray *locations = [NSMutableArray array];
+    for (NSInteger i = 0; i < count ; i ++) {
+        UIColor *color = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:0.3];
+        [colors addObject:(__bridge id)color.CGColor];
+        [locations addObject:@(i /(CGFloat)count)];
+    }
+    //颜色数组
+    _gradientLayer2.colors = colors;
+    //可以不设置
+    _gradientLayer2.locations = locations;
+    //startPoint endPoint 确定条纹方向 不设置 默认水平默认值[.5,0]和[.5,1]
+    _gradientLayer2.startPoint = CGPointMake(1, 0);
+    _gradientLayer2.endPoint = CGPointMake(0, 1);
+    
+    _gradientLayer2.type = kCAGradientLayerAxial;
+    
+    _gradientLayer2.frame = CGRectMake(0, 0, 375, 150);
+    
+    [self.view.layer addSublayer:_gradientLayer2];
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"colors"];
+    
+    animation.duration = 2;
+    
+    animation.repeatCount = MAXFLOAT;
+    
+    NSMutableArray *toValue = [NSMutableArray array];
+    
+    for (NSInteger i = 0; i < count ; i ++) {
+        
+        UIColor *color = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:0.3];
+        
+        [toValue addObject:(__bridge id)color.CGColor];
+        
+    }
+    animation.autoreverses = YES;
+    animation.toValue = toValue;
+    [_gradientLayer2 addAnimation:animation forKey:@"gradientLayer"];
+    
+}
+
 
 
 
